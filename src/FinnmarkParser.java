23e1528d0f7cd8f1bbd2536b0b3a8a307e42fadf
HktOverload <hktoverload@protonmail.com>
2022-05-26 20:40:44 -0400

    negative numbers exist

--- src/FinnmarkParser.java
+++ src/FinnmarkParser.java
@@ -12,10 +12,18 @@ public class FinnmarkParser {
                 return parseFormatted(s);
             case '\'':
                 return parseString(s);
+            case ':':
+                int n = readBinInt(s);
+                if (s.charAt(s.length() - 1) == 'a') return asIdx(n);
+                else return asNum(n);
             case '0': case '1': case '2': case '3': case '4':
             case '5': case '6': case '7': case '8': case '9':
-                if (s.charAt(s.length() - 1) == 'a') return parseIdx(s);
-                else return parseNum(s);
+            case '-':
+                if (s.charAt(s.length() - 1) == 'a') {
+                    return asIdx(readDecInt(s));
+                } else {
+                    return asNum(readDecDouble(s));
+                }
             default:
                 return parseSymb(s);
         }
