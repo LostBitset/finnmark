02df0c6ab9c168cabe4433b07d160adc903abb69
HktOverload <hktoverload@protonmail.com>
2022-05-28 13:05:24 -0400

    Begin quoting stuff

---
+++ src/FVal_QTD.java
@@ -0,0 +1,5 @@
+public class FVal_QTD implements FVal {
+    public FVal inner;
+
+    public FVal_QTD(FVal inner) { this.inner = inner; }
+}
---
+++ src/FVal_UNQ.java
@@ -0,0 +1,5 @@
+public class FVal_UNQ implements FVal {
+    public FVal inner;
+
+    public FVal_UNQ(FVal inner) { this.inner = inner; }
+}
