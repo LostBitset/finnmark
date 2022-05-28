565654ab2385c7a0f7b49e1a5874b93ec531c4f5
HktOverload <hktoverload@protonmail.com>
2022-05-27 21:27:10 -0400

    `Hello, null.` lmao

--- src/FVal_FMS.java
+++ src/FVal_FMS.java
@@ -1,8 +1,33 @@
+import java.util.ArrayList;
+
 public class FVal_FMS implements FVal {
     public String u;
     public String[] refs;
 
-    public FVal_FMS(String u) { this.u = u; }
+    public FVal_FMS(String finnmark_form) {
+        ArrayList<String> refs_ = new ArrayList<>();
+        StringBuilder uBuilder = new StringBuilder();
+        StringBuilder currRef = new StringBuilder();
+        boolean refMode = false;
+        for (int i = 0; i < finnmark_form.length(); i++) {
+            char c = finnmark_form.charAt(i);
+            if (refMode) {
+                if (c == '%') {
+                    refs_.add(currRef.toString());
+                    uBuilder.append("%s");
+                    currRef = new StringBuilder();
+                    refMode = false;
+                    continue;
+                }
+                currRef.append(c);
+                continue;
+            }
+            if (c == '%') refMode = true;
+            else uBuilder.append(c);
+        }
+        this.u = uBuilder.toString();
+        this.refs = refs_.toArray(String[]::new);
+    }
 
     public String toString()
     { return String.format("[fms]%s", this.u); }
--- src/FVal_JFN.java
+++ src/FVal_JFN.java
@@ -1,6 +1,6 @@
 import java.util.function.Function;
 
-public class FVal_JFN {
+public class FVal_JFN implements FVal {
     public Function<FVal[],FVal> lambda;
 
     public FVal_JFN(Function<FVal[],FVal> lambda) { this.lambda = lambda; }
